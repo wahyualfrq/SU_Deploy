b41aea7ab777d2c3dac4ab17a04167599eb084b3
wahyualfrq <wahyudialfurqon109@gmail.com>
2026-01-11 16:30:04 +0700

    vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+        \URL::forceScheme('https');
+    }
         View::share('menu', config('menu'));
     }
 }
